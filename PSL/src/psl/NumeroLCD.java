d3177d8b5c814e8936ea34063fea9a360681e68c
alejandrachacon <alejandra.kcm@gmail.com>
2017-09-27 00:25:35 -0500

    Last version

---
+++ PSL/src/psl/NumeroLCD.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psl;
+
+/**
+ *
+ * @author Ale
+ */
+public class NumeroLCD {
+    
+    private final int[] pf1;
+    private final int[] pf2;
+    private final int[] pf3;
+    private final int[] pf4;
+    private final int[] pf5;
+    
+      public NumeroLCD() {
+        // Inicializa variables
+        this.pf1 = new int[2];
+        this.pf2 = new int[2];
+        this.pf3 = new int[2];
+        this.pf4 = new int[2];
+        this.pf5 = new int[2];
+    }
+      
+    
+}
---
+++ PSL/test/psl/PslSuite.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psl;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Ale
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({psl.LCDTesterTest.class, psl.NumeroLCDTest.class, psl.ImpresorLCDTest.class})
+public class PslSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
